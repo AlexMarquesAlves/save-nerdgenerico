0b4a826a721fd4d882d8e6cff40c92227d11b1ca
¡Duque <atanaelleonardo@gmail.com>
2024-05-19 20:00:57 -0300

    feat: added online development

---
+++ main.js
@@ -0,0 +1,34 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+<div class="status-bar">
+  <div class="loading-bar">
+    <div class="progress"></div>
+  </div>
+  <p class="message">
+    NerdGenérico está em processo de salvamento, não desligue o console ou
+    retire o memory card!
+  </p>
+</div>
+
+<div class="success-popup" id="success-popup">
+  <p>Salvamento concluído! <span>NerdGenérico</span> salvo com sucesso</p>
+</div>
+`;
+
+const progressBar = document.querySelector('.progress');
+const successPopup = document.getElementById('success-popup');
+
+let progress = 0;
+
+export function updateProgress() {
+  progress += 1;
+  progressBar.style.width = `${progress}%`;
+  if (progress >= 100) {
+    setTimeout(() => {
+      successPopup.classList.add('show');
+    }, 1);
+  }
+}
+
+setInterval(updateProgress, 50);
